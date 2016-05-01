9821ccf1b6e732e4d534122a2ebb3b17cbb9cfe7
MarcosMWB <marcosmwb1919@gmail.com>
2016-05-01 02:35:26 -0300

    soma e media de 1 a 100

---
+++ cafe2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, soma, media;
+ 
+ for(i=1;i<=100;i++)
+ {
+     soma=soma+i;
+ }
+ media=soma/i;
+ printf("a soma eh %d e a media %d\n", soma, media);
+return 0;
+}
